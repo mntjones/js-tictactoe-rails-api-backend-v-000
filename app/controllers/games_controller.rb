3d47818916f503200f5c80cef845174896d44c95
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-13 21:13:38 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,33 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+    @games = Game.all
+  end
 
+  def new
+    
+  end
+
+  def create
+    @game = Game.new()
+    
+  end
+
+  def show
+    @game = Game.find(id: params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+    @game = Game.find(id: params[:id])
+  end
+
+  private
+  def game_params
+
+  end
+  
 end
